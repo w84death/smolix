e5b2142432dc9ebb9f56918fa3fb3d8cbab30032
Krzysztof Krystian Jankowski <kj@p1x.in>
2025-04-27 23:15:21 +0200

    alpha2

--- src/kernel.asm
+++ src/kernel.asm
@@ -7,25 +7,18 @@
 org 0x0000
 use16
 
-; Kernel consts
-PROMPT_MSG          equ '+'
-PROMPT_SYS_MSG      equ '>'
-PROMPT_STATUS       equ '.'
-PROMPT_ERR          equ '!'
-PROMPT_USR          equ '@'
+GLYPHS_START     equ 0x80
+PROMPT_MSG          equ GLYPHS_START+0xA
+PROMPT_SYS_MSG      equ GLYPHS_START+0x8
+PROMPT_STATUS       equ GLYPHS_START+0xD
+PROMPT_ERR          equ GLYPHS_START+0xB
+PROMPT_USR          equ GLYPHS_START+0xC
 PROMPT_SPACE        equ ' '
 PROMPT_CR           equ 0x0D
 PROMPT_LF           equ 0x0A
-GLYPH_SHIFT_IDX     equ 0x80
-
-; Entry point
-start:
-  ; Set up data segment correctly (important for booting from bootloader)
-  push cs            ; Push code segment
-  pop ds             ; Set data segment = code segment
-  push cs            ; Make sure ES is also set correctly
-  pop es
-  sti                ; Enable interrupts - critical for BIOS calls to work
+PROMPT_END          equ GLYPHS_START+0xF
+COLOR_PRIMARY       equ 0x1F ; White on blue 
+COLOR_SECONDARY     equ 0x0E ; Yellow on black
 
 ; System reset
 ; This function resets the system.
@@ -35,54 +28,30 @@ os_reset:
   ; Set up the video mode
   mov ah, 0x00		    ; Set video mode
 	mov al, 0x03		    ; 720x400 VGA text mode
-	int 0x10
-
-  mov al, 0
-  call os_load_glyph
-  inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-inc al
-  call os_load_glyph
-
-  ; Print the welcome message
-  call os_print_ver
-
-  ; Print the copyright message
-  mov bl, PROMPT_MSG
-  call os_print_prefix
-  mov si, copyright_msg
-  call os_print_str
-
-  ; Print the more info message
-  mov bl, PROMPT_MSG
-  call os_print_prefix
-  mov si, more_info_msg
-  call os_print_str
+	int 0x10            ; 80x25 text mode
+  
+  call os_load_all_glyphs
+  call os_clear_screen
+  call os_print_welcome
  
 ; Main system loop
+; This is the main loop of the operating system.
+; It waits for user input and interprets it.
+; Expects: None
+; Returns: None
 os_main_loop:
 
-  mov bl, PROMPT_USR
-  call os_print_prefix
-
+  call os_print_prompt
   call os_get_key
   call os_print_chr
   call os_interpret_char
   
 jmp os_main_loop
 
+; Print help message
+; This function prints the help message to the screen.
+; Expects: None
+; Returns: None
 os_print_help:
   mov bl, PROMPT_MSG
   call os_print_prefix
@@ -129,6 +98,40 @@ os_print_ver:
   call os_print_str
 ret
 
+; Print welcome message
+; This function prints the welcome message to the screen.
+; Expects: None
+; Returns: None
+os_print_welcome:
+  mov bl, PROMPT_SYS_MSG
+  call os_print_prefix
+  mov si, welcome_msg
+  call os_print_str
+
+  call os_print_ver
+
+  ; Print the copyright message
+  mov bl, PROMPT_MSG
+  call os_print_prefix
+  mov si, copyright_msg
+  call os_print_str
+
+  ; Print the more info message
+  mov bl, PROMPT_MSG
+  call os_print_prefix
+  mov si, more_info_msg
+  call os_print_str
+ret
+
+; Print prompt
+; This function prints the prompt to the screen.
+; Expects: None
+; Returns: None
+os_print_prompt:
+  mov bl, PROMPT_USR
+  call os_print_prefix
+ret
+
 ; Print character
 ; This function prints a character to the screen.
 ; Expects: AL = character to print
@@ -136,6 +139,30 @@ ret
 os_print_chr:
   mov ah, 0x0e    ; BIOS teletype output function
   int 0x10        ; BIOS teletype output function
+ret
+
+; Print character with color
+; This function prints a character to the screen with a specific color.
+; Expects: AL = character to print
+;          BL = color attribute
+; Returns: None
+os_print_chr_color:
+  mov bh, 0x00       ; Page number
+  mov ah, 0x09       ; BIOS function to write character and attribute
+  mov cx, 1          ; Number of times to write character
+  int 0x10           ; BIOS video interrupt
+ret
+
+; Print character with color and multiplication
+; This function prints a character to the screen with a specific color and multiplies it.
+; Expects: AL = character to print
+;          BL = color attribute
+;          CX = number of times to print
+; Returns: None
+os_print_chr_color_mul:
+  mov bh, 0x00       ; Page number
+  mov ah, 0x09       ; BIOS function to write character and attribute
+  int 0x10           ; BIOS video interrupt
 ret
 
 ; Print string
@@ -152,9 +179,50 @@ os_print_str:
     int 0x10          ; BIOS video interrupt
   jmp near .next_char
   .terminated:
+
+  mov al, PROMPT_END
+  int 0x10
 ret
 
+; Clear screen
+; This function clears the screen with primary colors.
+; Expects: None
+; Returns: None
+os_clear_screen:
+  call os_cursor_pos_reset
+  mov ax, 0x0600     ; Function 06h (scroll window up)
+  mov bh, COLOR_PRIMARY  ; Set color attribute
+  mov cx, 0x0000     ; Top left corner (row 0, col 0)
+  mov dx, 0x184F     ; Bottom right corner (row 24, col 79)
+  int 0x10
+  call os_cursor_pos_reset
+ret
+
+os_cursor_pos_reset:
+  xor dx, dx
+  mov ah, 0x2
+  mov bh, 0x0
+  int 0x10
+ret
+
+; Set cursor position
+; This function sets the cursor position on the screen.
+; Expects: DX = position (row * 80 + col)
+; Returns: None
+os_cursor_set_pos:
+  mov ah, 0x02
+  mov bh, 0x00
+  int 0x10
+ret
+
+; Set color
+; This function sets the color of the text on the screen.
+; Expects: BL = color attribute
+; Returns: None
 os_set_color:
+  mov ah, 0x0B
+  mov bh, 0x00
+  int 0x10
 ret
 
 ; Load glyph
@@ -170,27 +238,54 @@ os_load_glyph:
   mov bp, [si]         ; Get address of glyph data
   push ds
   pop es              ; Ensure ES = DS (BIOS expects ES:BP for font data)
-  mov ax, 1100h       ; BIOS function to load user-defined font
-  mov bh, 10h         ; Number of bytes per character (16 for 8x16 glyph)
+  mov ax, 1100h       ; BIOS function to load 9×16 user-defined font
+  mov bh, 10h         ; Number of bytes per character (16 for 8/9×16 glyph)
   mov bl, 00h         ; RAM block (0 for default)
-  mov cx, 01h         ; Number of characters to replace (1 for now)
+  mov cx, 0x01         ; Number of characters to replace (1 for now)
   pop dx
-  add dx, GLYPH_SHIFT_IDX ; Adjust character code for extended ASCII
+  add dx, GLYPHS_START ; Adjust character code for extended ASCII
   int 10h             ; Call BIOS video interrupt to load the font
   popa
 ret
 
-
+; Load all glyphs
+; This function loads all custom glyphs into the VGA font memory using BIOS.
+; Expects: None
+; Returns: None
 os_load_all_glyphs:
+  mov si, glyph_table
+  xor cx, cx
+  .loop_glyphs:
+    lodsw
+    cmp ax, 0x0
+    je .done
+    mov ax, cx
+    call os_load_glyph
+    inc cx
+    jmp .loop_glyphs
+  .done:
 ret
 
-os_draw_glyph:
-ret
-
-os_draw_glyph_long:
-ret
-
+; Draw window
+; This function draws a window on the screen.
+; Expects: DH/DL = window position (row/column)
+; CH/CL = window size (height/width)
+; Returns: None
 os_draw_window:
+  call os_cursor_set_pos
+  
+  xor ch, ch
+  .line_loop:
+    push cx
+    mov al, 7+GLYPHS_START
+    mov cl, 0x10
+    call os_print_chr_color_mul
+    inc dh
+    call os_cursor_set_pos
+    pop cx
+  loop .line_loop
+
+
 ret
 
 ; Wait for key press
@@ -216,6 +311,11 @@ os_interpret_char:
   cmp al, "d"
   je .print_debug
 
+  mov bl, PROMPT_ERR
+  call os_print_prefix
+  mov si, unknown_cmd_msg
+  call os_print_str
+
   jmp .done
 
   .print_os_version:
@@ -232,13 +332,18 @@ os_interpret_char:
   .print_debug:
     mov bl, PROMPT_MSG
     call os_print_prefix
-    mov al, GLYPH_SHIFT_IDX
+    mov al, GLYPHS_START
     call os_print_chr
     mov cx, 0xF
     .loop_chars:
       inc al
       call os_print_chr
     loop .loop_chars
+
+    mov bl, PROMPT_MSG
+    call os_print_prefix
+    mov si, hex_ruler_msg
+    call os_print_str
     jmp .done
 
 
@@ -246,12 +351,78 @@ os_interpret_char:
 ret
 
 ; Data section
-version_msg         db 'SMOLiX Version alpha1d', 0
-welcome_msg         db 'Welcome to SMOLiX Operating System', 0
+version_msg         db 'Version alpha2', 0
+welcome_msg         db 'Welcome to ', 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, ' Operating System', 0
 copyright_msg       db '(C) 2025 Krzysztof Krystian Jankowski', 0
 more_info_msg       db 'Type "h" for help. Read more at smol.p1x.in/smolix/', 0
 help_os_msg         db 'Legend: ">" system message, "+" message, "." return status, "@" user prompt', 0
 help_cmds_msg       db 'Commands: "v" version, "r" reset, "d" debug', 0
+unknown_cmd_msg     db 'Unknown command', 0
+hex_ruler_msg       db '0123456789ABCDEF', 0
+
+glyph_00:
+    db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+glyph_01:
+    db 0x00, 0x00, 0x00, 0x3f, 0x7f, 0xf7, 0x70, 0x3e
+    db 0xcf, 0xe7, 0xff, 0xfe, 0x7c, 0x00, 0x00, 0x00
+
+glyph_02:
+    db 0x00, 0x00, 0x00, 0xe3, 0xe3, 0xe3, 0xf7, 0xf7
+    db 0xff, 0xdd, 0xc9, 0xc1, 0xc1, 0x00, 0x00, 0x00
+
+glyph_03:
+    db 0x00, 0x00, 0x00, 0xc3, 0xc7, 0xcd, 0xdc, 0xdc
+    db 0xdc, 0xce, 0xcf, 0xc7, 0xc3, 0x00, 0x00, 0x00
+
+glyph_04:
+    db 0x00, 0x00, 0x00, 0x0c, 0x8c, 0xcc, 0xec, 0xec
+    db 0xec, 0xcc, 0xcf, 0x8f, 0x0f, 0x00, 0x00, 0x00
+
+glyph_05:
+    db 0x00, 0x00, 0x00, 0x77, 0x77, 0x07, 0x77, 0x77
+    db 0x71, 0x77, 0x77, 0x77, 0x77, 0x00, 0x00, 0x00
+
+glyph_06:
+    db 0x00, 0x00, 0x00, 0x70, 0x70, 0x70, 0xf0, 0xf0
+    db 0xe0, 0xf0, 0x70, 0x70, 0x70, 0x00, 0x00, 0x00
+
+glyph_07:
+    db 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+    db 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+
+glyph_08:
+    db 0x00, 0x00, 0x00, 0x00, 0x54, 0x81, 0x3c, 0xad
+    db 0x3c, 0xbd, 0x00, 0x55, 0x00, 0x00, 0x00, 0x00
+
+glyph_09:
+    db 0x00, 0x00, 0x18, 0x6c, 0xec, 0xf2, 0x7f, 0x1f
+    db 0x40, 0x6a, 0x7e, 0x36, 0x1c, 0x00, 0x00, 0x00
+
+glyph_0a:
+    db 0x00, 0x00, 0x00, 0xfc, 0xfe, 0x8b, 0xff, 0xa1
+    db 0xff, 0x85, 0xff, 0xa1, 0xff, 0xff, 0x00, 0x00
+
+glyph_0b:
+    db 0x00, 0x0c, 0x14, 0x16, 0x1e, 0x1e, 0x0c, 0x0c
+    db 0x0c, 0x08, 0x00, 0x18, 0x38, 0x10, 0x00, 0x00
+
+glyph_0c:
+    db 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x40, 0x44
+    db 0x46, 0x3f, 0x06, 0x04, 0x00, 0x00, 0x00, 0x00
+
+glyph_0d:
+    db 0x00, 0x00, 0x00, 0x00, 0x7e, 0x81, 0xad, 0x81
+    db 0x7f, 0x06, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00
+
+glyph_0e:
+    db 0x00, 0x00, 0x3c, 0x7e, 0xff, 0xff, 0x7e, 0xbd
+    db 0xc3, 0xff, 0xff, 0xff, 0x7e, 0x3c, 0x00, 0x00
+
+glyph_0f:
+    db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    db 0x00, 0x00, 0x50, 0x20, 0x50, 0x00, 0x00, 0x00
 
 glyph_table:
     dw glyph_00
@@ -263,44 +434,13 @@ glyph_table:
     dw glyph_06
     dw glyph_07
     dw glyph_08
+    dw glyph_09
+    dw glyph_0a
+    dw glyph_0b
+    dw glyph_0c
+    dw glyph_0d
+    dw glyph_0e
+    dw glyph_0f
     dw 0x0
-
-glyph_00:
-    db 0x00, 0x00, 0x08, 0x18, 0x38, 0x78, 0xF8, 0x78
-    db 0x38, 0x18, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00
-
-glyph_01:
-    db 0x00, 0x00, 0x00, 0x0f, 0x1f, 0x3d, 0x1c, 0x0f
-    db 0x33, 0x39, 0x3f, 0x3f, 0x1f, 0x00, 0x00, 0x00
-
-glyph_02:
-    db 0x00, 0x00, 0x00, 0xdc, 0xdc, 0xdc, 0x1e, 0x9e
-    db 0xdf, 0xdb, 0xd9, 0x98, 0x18, 0x00, 0x00, 0x00
-
-glyph_03:
-    db 0x00, 0x00, 0x00, 0x78, 0x79, 0x7b, 0xff, 0xff
-    db 0xff, 0xbb, 0x3b, 0x39, 0x38, 0x00, 0x00, 0x00
-
-glyph_04:
-    db 0x00, 0x00, 0x00, 0xe3, 0xf3, 0x3b, 0x1f, 0x1f
-    db 0x1f, 0x9b, 0xfb, 0xf3, 0xe3, 0x00, 0x00, 0x00
-
-glyph_05:
-    db 0x00, 0x00, 0x00, 0x0e, 0x0e, 0x00, 0x0e, 0x0e
-    db 0x0e, 0x0e, 0xee, 0xee, 0xee, 0x00, 0x00, 0x00
-
-glyph_06:
-    db 0x00, 0x00, 0x00, 0xdc, 0xdc, 0xdc, 0xfc, 0xfc
-    db 0x78, 0xfc, 0xdc, 0xdc, 0xdc, 0x00, 0x00, 0x00
-
-glyph_07:
-    db 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-    db 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-
-glyph_08:
-    db 0x7f, 0x80, 0xbf, 0xbf, 0xbf, 0xbf, 0xbf, 0xbf
-    db 0xbf, 0xbf, 0xbf, 0xbf, 0xbf, 0xbf, 0xbf, 0xbf
-
-
 
 db "P1X"            ; Use HEX viewer to see P1X at the end of binary
---
+++ tool/tileset2glyphs.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+# SMOLiX Tileset to Glyphs Converter
+# This tool converts a PNG tileset with 1-bit color sprites
+# into byte arrays suitable for VGA font glyphs in SMOLiX OS.
+# Copyright (C) 2025 Krzysztof Krystian Jankowski
+
+import sys
+from PIL import Image
+
+def usage():
+    print("SMOLiX Tileset to Glyphs Converter")
+    print("Usage: python tileset2glyphs.py <input_file> [output_file] [options]")
+    print("Options:")
+    print("  -h, --help     Show this help message")
+    print("  -a, --asm      Output assembly format (default)")
+    print("  -b, --binary   Output binary format")
+    print("  -n <num>       Number of sprites to process (default: all)")
+    print("  -w <width>     Width of each sprite (default: 8)")
+    print("  -t <height>    Height of each sprite (default: 16)")
+    sys.exit(1)
+
+def pixel_to_bit(pixel):
+    # For 1-bit color, consider non-zero/non-black pixels as '1'
+    if isinstance(pixel, tuple):
+        # RGB or RGBA format
+        return 1 if sum(pixel[:3]) > 0 else 0
+    else:
+        # Grayscale
+        return 1 if pixel > 0 else 0
+
+def convert_sprite_to_bytes(img, sprite_index, width, height, sprites_per_row):
+    sprite_bytes = []
+    
+    # Calculate sprite position in a grid layout
+    row = sprite_index // sprites_per_row
+    col = sprite_index % sprites_per_row
+    
+    # Calculate starting pixel coordinates
+    start_x = col * width
+    start_y = row * height
+    
+    # Process each row of the sprite
+    for y in range(height):
+        byte_value = 0
+        
+        # Process each pixel in the row (from left to right)
+        for x in range(width):
+            pixel = img.getpixel((start_x + x, start_y + y))
+            bit = pixel_to_bit(pixel)
+            # Shift and set the bit (MSB first)
+            byte_value = (byte_value << 1) | bit
+        
+        sprite_bytes.append(byte_value)
+    
+    return sprite_bytes
+
+def generate_asm_output(sprites_data, output_file=None):
+    output = []
+    
+    # Generate the assembly code for each sprite
+    for i, sprite in enumerate(sprites_data):
+        output.append(f"glyph_{i:02x}:")
+        
+        # Format bytes in rows of 8
+        rows = [sprite[j:j+8] for j in range(0, len(sprite), 8)]
+        for row in rows:
+            byte_str = ", ".join([f"0x{b:02x}" for b in row])
+            output.append(f"    db {byte_str}")
+        
+        output.append("")
+    
+    # Add a table that contains pointers to all glyphs
+    output.append("glyph_table:")
+    for i in range(len(sprites_data)):
+        output.append(f"    dw glyph_{i:02x}")
+    
+    result = "\n".join(output)
+    
+    if output_file:
+        with open(output_file, 'w') as f:
+            f.write(result)
+        print(f"Assembly output written to {output_file}")
+    else:
+        print(result)
+
+def generate_binary_output(sprites_data, output_file=None):
+    if not output_file:
+        print("Error: Binary output requires an output file")
+        sys.exit(1)
+    
+    with open(output_file, 'wb') as f:
+        # Write the number of sprites
+        f.write(bytes([len(sprites_data)]))
+        
+        # Write each sprite's data
+        for sprite in sprites_data:
+            f.write(bytes(sprite))
+    
+    print(f"Binary output written to {output_file}")
+
+def main():
+    # Default parameters
+    input_file = None
+    output_file = None
+    output_format = "asm"
+    sprite_width = 8
+    sprite_height = 16
+    num_sprites = None
+    
+    # Parse command line arguments
+    args = sys.argv[1:]
+    i = 0
+    while i < len(args):
+        arg = args[i]
+        if arg in ['-h', '--help']:
+            usage()
+        elif arg in ['-a', '--asm']:
+            output_format = "asm"
+        elif arg in ['-b', '--binary']:
+            output_format = "bin"
+        elif arg == '-n' and i + 1 < len(args):
+            num_sprites = int(args[i + 1])
+            i += 1
+        elif arg == '-w' and i + 1 < len(args):
+            sprite_width = int(args[i + 1])
+            i += 1
+        elif arg == '-t' and i + 1 < len(args):
+            sprite_height = int(args[i + 1])
+            i += 1
+        elif input_file is None:
+            input_file = arg
+        elif output_file is None:
+            output_file = arg
+        i += 1
+    
+    if input_file is None:
+        usage()
+    
+    try:
+        img = Image.open(input_file)
+    except Exception as e:
+        print(f"Error opening image: {e}")
+        sys.exit(1)
+    
+    # Calculate number of sprites in the image
+    img_width, img_height = img.size
+    sprites_per_row = img_width // sprite_width
+    sprites_per_col = img_height // sprite_height
+    max_sprites = sprites_per_row * sprites_per_col
+    
+    if num_sprites is None:
+        num_sprites = max_sprites
+    else:
+        num_sprites = min(num_sprites, max_sprites)
+    
+    print(f"Image dimensions: {img_width}x{img_height}")
+    print(f"Sprite size: {sprite_width}x{sprite_height}")
+    print(f"Sprite grid: {sprites_per_row} columns x {sprites_per_col} rows")
+    print(f"Processing {num_sprites} sprites from {input_file}")
+    
+    # Convert sprites to byte arrays
+    sprites_data = []
+    for i in range(num_sprites):
+        sprite_bytes = convert_sprite_to_bytes(img, i, sprite_width, sprite_height, sprites_per_row)
+        sprites_data.append(sprite_bytes)
+        
+        # Calculate the sprite's position for better progress reporting
+        row = i // sprites_per_row + 1
+        col = i % sprites_per_row + 1
+        print(f"Sprite {i+1}/{num_sprites} (row {row}, column {col}) converted")
+    
+    # Generate output
+    if output_format == "asm":
+        generate_asm_output(sprites_data, output_file)
+    elif output_format == "bin":
+        generate_binary_output(sprites_data, output_file)
+
+if __name__ == "__main__":
+    main()
