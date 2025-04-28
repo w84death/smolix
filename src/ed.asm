aeaf21aa282488d097b2717198b86b6bc4eca750
Krzysztof Krystian Jankowski <kj@p1x.in>
2025-04-28 21:06:34 +0200

    beginning of alpha3

--- src/ed.asm
+++
@@ -1,4 +0,0 @@
-; SMOLiX: Real Mode, Raw Power.
-; This is the text editor.
-; Copyright (C) 2025 Krzysztof Krystian Jankowski
-; This program is free software. See LICENSE for details.
--- src/sh.asm
+++
@@ -1,4 +0,0 @@
-; SMOLiX: Real Mode, Raw Power.
-; This is a shell.
-; Copyright (C) 2025 Krzysztof Krystian Jankowski
-; This program is free software. See LICENSE for details.
